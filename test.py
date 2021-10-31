b14cb2c75d8fc44ceb25d0e57ba7c6f5ca1a8904
kptis <shinkhong97@gmail.com>
2021-10-31 23:23:45 +0700

    h

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
-name 
+from flask import Flask, render_template,request, redirect, url_for
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/submit', methods=['POST','GET'])
+def submit():
+    if request.method == 'POST':
+        text = request.form['text_area']
+        if text != "":
+            print(text)
+            return redirect(url_for('home'))
+        else:
+            print("please enter into text area")
+            return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
