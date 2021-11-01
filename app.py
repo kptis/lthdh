72792797dcaf91445da7b46df0611ff268bf7c56
kptis <shinkhong97@gmail.com>
2021-11-01 08:51:11 +0700

    update

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
 from os import pardir
 from flask import Flask, render_template,request, redirect, url_for
+from flask.helpers import flash
 from model.main import predict
 
 app = Flask(__name__)
+
+app.secret_key = "btlthdh"
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/submit', methods=['POST','GET'])
+@app.route('/', methods=['POST'])
 def submit():
     if request.method == 'POST':
         text = request.form['text_area']
         if text != "":
-            print(text)
-            print(predict(text))
-            return redirect(url_for('home'))
+            print(text)       
+            return render_template('home.html', text = text)
         else:
+            flash("Điền nội dung vào mới dự đoán được chứ ơ hay -.-")
             print("please enter into text area")
             return redirect(url_for('home'))
 
